5fd8f3da0fc32cdee8077d36ef1974a3bb76c5de
Jorge Ballester <jorge.ballcor@gmail.com>
2025-06-23 18:25:55 +0200

    fix: DB url import

--- db/engine.py
+++ db/engine.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import os
+from common.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True, pool_recycle=3600)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
