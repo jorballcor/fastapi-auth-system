bb330da2fca4a2597ca553bb3f19d3de8943bbfa
Jorge Ballester <jorge.ballcor@gmail.com>
2025-07-17 16:28:15 +0200

    add: test conifg

--- common/config.py
+++ common/config.py
@@ -1,8 +1,11 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 
 class Settings(BaseSettings):
+    TESTING: bool = False
+    
     DATABASE_URL: str = Field(..., env="DATABASE_URL")
 
     SECRET_KEY: str = Field(..., min_length=32, env="SECRET_KEY")
@@ -15,7 +18,9 @@ class Settings(BaseSettings):
     FIRST_SUPERUSER_EMAIL: str = Field(..., env="FIRST_SUPERUSER_EMAIL")
     FIRST_SUPERUSER_PASSWORD: str = Field(..., env="FIRST_SUPERUSER_PASSWORD")
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=".env.test" if os.getenv("TESTING") == "1" else ".env",
+    )
 
 
 settings = Settings()
