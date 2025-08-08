a30c801b81e9de5455eb267ba6cd213b9b63326a
Jorge Ballester <jorge.ballcor@gmail.com>
2025-08-08 20:08:23 +0200

    fix: HTTPExceptions

--- db/exceptions.py
+++ db/exceptions.py
@@ -1,4 +1,6 @@
-class UserNotFoundException(Exception):
+from fastapi import HTTPException, status
+
+class UserNotFoundException(HTTPException):
     """Exception raised when a user is not found in the database."""
 
     def __init__(self, username: str):
@@ -6,8 +8,11 @@ class UserNotFoundException(Exception):
         super().__init__(f"User '{username}' not found in the database.")
 
 
-class DatabaseConnectionError(Exception):
+class DatabaseConnectionError(HTTPException):
     """Exception raised when there is a database connection error."""
 
     def __init__(self, message: str = "Could not connect to the database."):
-        super().__init__(message)
+        super().__init__(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail=message
+        )
--- todos/exceptions.py
+++ todos/exceptions.py
@@ -1,30 +1,45 @@
-class TodoAlreadyExistsException(Exception):
+from fastapi import HTTPException, status
+
+
+class TodoAlreadyExistsException(HTTPException):
     """Exception raised when a Todo task already exists in the database."""
 
     def __init__(self, title: str):
         self.title = title
-        super().__init__(f"Todo '{title}' already exists in the database.")
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail=f"Todo '{title}' already exists in the database."
+        )
 
 
-class UserTodoNotFoundException(Exception):
+class UserTodoNotFoundException(HTTPException):
     """Exception raised when no todo is found for a user."""
 
     def __init__(self, user_id: int):
         self.user_id = user_id
-        super().__init__(f"No todo found for user with ID {user_id}.")
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"No todo found for user with ID {user_id}."
+        )
         
 
-class DeleteTodoException(Exception):
+class DeleteTodoException(HTTPException):
     """Exception raised when a todo cannot be deleted."""
 
     def __init__(self, todo_id: int):
         self.todo_id = todo_id
-        super().__init__(f"Todo with ID {todo_id} could not be deleted.")
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Todo with ID {todo_id} could not be deleted."
+        )
         
 
-class UpdateTodoException(Exception):
+class UpdateTodoException(HTTPException):
     """Exception raised when a todo cannot be updated."""
 
     def __init__(self, todo_id: int):
         self.todo_id = todo_id
-        super().__init__(f"Todo with ID {todo_id} could not be updated.")
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Todo with ID {todo_id} not found, it could not be updated."
+        )
